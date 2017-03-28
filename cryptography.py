c241579f8b0b976b4433e69ea9cbf70330bb4d35
Earljb <earlbarrowes@hanovernorwichschools.org>
2017-03-28 14:17:56 -0400

    Updated from Brython Server: 3/28/2017 2:17:56 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Earl
+Credit: Sam
 
 Assignment:
 
@@ -10,3 +10,14 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+en=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+message=input("Message: ")
+key=input("Key: ")
+
+associations[index]
+
+if "e":
+    print(" ")
+elif "q":
+    print("Goodbye!")
