86613b2d9df4e032599dde79e40347e254c8f53e
Earljb <earlbarrowes@hanovernorwichschools.org>
2017-03-31 14:36:23 -0400

    Updated from Brython Server: 3/31/2017 2:36:22 PM

--- cryptography.py
+++ cryptography.py
@@ -5,9 +5,23 @@ Credit: Sam
 
 Assignment:
 
+Enter e to encrypt, d to decrypt, or q to quit: z
+Did not understand command, try again.
+Enter e to encrypt, d to decrypt, or q to quit: e
+Message: Two plus two = Five
+Key: Lorem ipsum
++KF;B(CH=NIZ}m;R\Dt
+Enter e to encrypt, d to decrypt, or q to quit: d
+Message: +KF;B(CH=NIZ}m;R\Dt
+Key: Lorem ipsum
+Two plus two = Five
+Enter e to encrypt, d to decrypt, or q to quit: q
+Goodbye!
+
 Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
+"""
 """
 it = True
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
@@ -55,5 +69,61 @@ while it:
     elif en == 'q':
         print('Goodbye!')
         it = False    
+    else:
+        print("Did not understand command, try again.")
+"""
+
+on = True
+associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+ # make list of numbers for message and key
+ # add them while compiling a new list
+ # print new string
+ 
+ 
+ 
+while on:
+    command = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    if command == 'e':
+        m = input("Message: ")
+        k = input("Key: ")
+        mlist = []
+        klist = []
+        encryptionlist = []
+        encryption = []
+         
+         
+        for i in range(0,len(m)):
+            mlist.append(associations.find(m[i]))# creates mlist
+        for i in range(0,len(k)):
+            klist.append(associations.find(k[i]))# creates klist
+        for i in range(0,len(mlist)):#creates encryptionlist
+            encryptionlist.append(mlist[i]+klist[i%len(klist)])
+        for i in range(0,len(encryptionlist)):
+            encryption.append(associations[encryptionlist[i]%len(associations)])
+        print(''.join(encryption))
+         
+    elif command == 'd':
+        m = input("Message: ")
+        k = input("Key: ")
+        mlist = []
+        klist = []
+        decryptionlist = []
+        decryption = []
+         
+         
+        for i in range(0,len(m)):
+            mlist.append(associations.find(m[i]))# creates mlist
+        for i in range(0,len(k)):
+            klist.append(associations.find(k[i]))# creates klist
+        for i in range(0,len(mlist)):#creates decryptionlist
+            decryptionlist.append(mlist[i]-klist[i%len(klist)])
+        for i in range(0,len(decryptionlist)):
+            decryption.append(associations[decryptionlist[i]%len(associations)])
+        print(''.join(decryption))
+         
+         
+    elif command == 'q':
+        print('Goodbye!')
+        on = False    
     else:
         print("Did not understand command, try again.")
