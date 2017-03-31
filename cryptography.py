89a3ff3bfef82c1dba2815573e385b0e2743b8e5
Earljb <earlbarrowes@hanovernorwichschools.org>
2017-03-31 14:29:00 -0400

    Updated from Brython Server: 3/31/2017 2:28:59 PM

--- cryptography.py
+++ cryptography.py
@@ -9,19 +9,51 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
+it = True
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-en=input("Enter e to encrypt, d to decrypt, or q to quit: ")
-message=input("Message: ")
-key=input("Key: ")
 
 
-if en is "e":
-    for a in associations:
-        associations.find(associations)
-        print(associations.find(a))
-elif en is "d":
-    print("d")
-elif en in "q":
-    print("Goodbye!")
-else:
-    print("Did not understand command, try again.")
+
+while it:
+    en=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    if en=='e':
+        m=input("Message: ")
+        k=input("Key: ")
+        mlist=[]
+        klist=[]
+        elist=[]
+        e=[]
+        
+        for x in range(0,len(m)):
+            mlist.append(associations.find(m[x]))
+        for x in range(0,len(k)):
+            klist.append(associations.find(k[x]))
+        for x in range(0,len(mlist)):
+            elist.append(mlist[x]+klist[x%len(klist)])
+        for x in range(0,len(elist)):
+            e.append(associations[elist[x]%len(associations)])
+        print(''.join(e))
+        
+    elif en == 'd':
+        m=input("Message: ")
+        k=input("Key: ")
+        list=[]
+        klist=[]
+        dlist=[]
+        d=[]
+        
+        for x in range(0,len(m)):
+            mlist.append(associations.find(m[x]))
+        for x in range(0,len(k)):
+            klist.append(associations.find(k[x]))
+        for x in range(0,len(mlist)):
+            dlist.append(mlist[x]-klist[x%len(klist)])
+        for x in range(0,len(dlist)):
+            d.append(associations[dlist[x]%len(associations)])
+        print(''.join(d))
+        
+    elif en == 'q':
+        print('Goodbye!')
+        it = False    
+    else:
+        print("Did not understand command, try again.")
